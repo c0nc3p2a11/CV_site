a2c36692b36b2235020ee3133f8e9b6b7bafd3f1
Oleg Kiselyov <c0ncep2all@proton.me>
2022-11-18 18:17:41 +0600

    static connected

--- resume/main/views.py
+++ resume/main/views.py
@@ -2,13 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    template = 'main/index.html'
-    # posts = Post.objects.order_by('-pub_date')[:10]
-    # title = 'Это главная страница проекта Yatube'
-    # text = 'Последние обновления на сайте'
-    # context = {
-    #     'posts': posts,
-    #     'title' : title,
-    #     'text': text
-    # }
-    # return render(request, template, context)
+    return render(request, "index.html")
